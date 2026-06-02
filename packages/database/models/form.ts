fbfb076c38b0227ba960744c0a4e00ec02e26244
avipowar <powaravinash06/2gmail.com>
2026-06-02 12:17:44 +0530

    create form basic strture

---
+++ packages/database/models/form.ts
@@ -0,0 +1,14 @@
+import { pgTable, uuid, varchar, timestamp, boolean, text } from "drizzle-orm/pg-core";
+import { usersTable } from "./user";
+
+export const formsTable = pgTable("forms", {
+  id: uuid("id").primaryKey().defaultRandom(),
+
+  title: varchar("title", { length: 45 }).notNull(),
+  description: varchar("discription", { length: 300 }),
+
+  createdBy: uuid("created_by").references(() => usersTable.id),
+
+  createdAt: timestamp("created_at").defaultNow(),
+  updatedAt: timestamp("updated_at").$onUpdate(() => new Date()),
+});
