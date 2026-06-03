8ec18f27ec9ba20041bd281769845c49fba00bf7
avipowar <powaravinash06/2gmail.com>
2026-06-03 13:48:24 +0530

    write useListForms hook to frontend

--- apps/web/hooks/api/form/index.ts
+++ apps/web/hooks/api/form/index.ts
@@ -29,3 +29,23 @@ export const useCreateForm = () => {
     status,
   };
 };
+
+export const useListForms = () => {
+  const {
+    data: forms,
+    error,
+    isFetched,
+    isFetching,
+    isLoading,
+    status,
+  } = trpc.form.listForms.useQuery();
+
+  return {
+    forms,
+    error,
+    isFetched,
+    isFetching,
+    isLoading,
+    status,
+  };
+};
